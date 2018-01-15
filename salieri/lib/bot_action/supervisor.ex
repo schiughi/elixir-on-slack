f07d0c087e84377a3edd48a6c5d7d315f486a505
schiughi <schiughi@gmail.com>
2018-01-16 01:25:39 +0900

    add bot manager

---
+++ salieri/lib/bot_action/supervisor.ex
@@ -0,0 +1,14 @@
+defmodule BotAction.Supervisor do
+  def start_link(opts \\ []) do
+    Task.Supervisor.start_link(opts)
+  end
+
+  def start_action(supervisor, command, trigger, message, slack) do
+    Task.Supervisor.start_child(supervisor, fn ->
+      case command do
+        :respond -> BotAction.Action.respond(trigger, message, slack)
+        :hear -> BotAction.Action.hear(trigger, message, slack)
+      end
+    )
+  end
+end
